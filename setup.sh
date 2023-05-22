4bb2aa829764e9f89061d7d0d6cd28c9a35997d8
Anton Drasbæk <202008161@post.au.dk>
2023-05-22 08:49:44 +0200

    added echos

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # install requirements
+echo "[INFO] Installing requirements..."
 python3 -m pip install -r requirements.txt
 
 # download spacy models
+echo "[INFO] Downloading spacy models..."
 python3 -m spacy download en_core_web_md
 python3 -m spacy download en_core_web_sm
 python3 -m spacy download en_core_web_trf
